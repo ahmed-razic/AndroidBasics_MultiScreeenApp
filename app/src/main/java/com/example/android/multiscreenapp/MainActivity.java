53d291807fdc6e4940f755c862c460fe48d456b9
ahmed-razic <ahmed.razic@gmail.com>
2021-01-26 22:05:56 +0100

    added activities

--- app/src/main/java/com/example/android/multiscreenapp/MainActivity.java
+++ app/src/main/java/com/example/android/multiscreenapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android.multiscreenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersActivity(View view) {
+        Intent openNumbers = new Intent(view.getContext(), NumbersActivity.class);
+        startActivity(openNumbers);
     }
 }
